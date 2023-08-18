f2a14a5a9bda835c148f6094da561a5103d1db9f
RichardSkartveit <114581981+RichardSkartveit@users.noreply.github.com>
2023-08-18 14:12:52 +0200

    ongoing

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const url = 'https://noroff-api-docs.netlify.app/basic-endpoints/jokes'
-
-async function getData(){
- const response = await fetch(url)
- console.log(response)
+async function fetchJokes(category) {
+    try {
+        const response = await fetch('https://api.noroff.dev/api/v1/jokes?category=${category}')
+        const data = await response.json()
+        return data;
+    } catch (error) {
+        console.error('Error fetching jokes:', error);
+    }
 }
 
-getData()
+document.addEventListener('DOMContentLoaded' async function() {
+    const jokesIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const jokes = await fetchJokes(jokesIds);
+})
