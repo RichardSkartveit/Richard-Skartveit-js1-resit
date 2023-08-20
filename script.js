f17c7e7673ac44be500f36379f292d721d142689
RichardSkartveit <114581981+RichardSkartveit@users.noreply.github.com>
2023-08-20 22:21:39 +0200

    short but sweet

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function fetchJokes(jokeIds) {
 }
 
 document.addEventListener('DOMContentLoaded', async function() {
-    const jokeIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+    const jokeIds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
 
     const jokes = await fetchJokes(jokeIds);
 
